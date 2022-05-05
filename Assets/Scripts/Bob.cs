ebeb62c74fd4ed253141691b591a032eb5bc9ccf
ArvinChau <89488993+ArvinChau@users.noreply.github.com>
2022-05-05 15:39:13 -0700

    fini

---
+++ Assets/Scripts/Bob.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour
+{
+    float speed = 5f;
+    float height = 100f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        float newY = Mathf.Sin(Time.time * speed);
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
+    }
+
+}
